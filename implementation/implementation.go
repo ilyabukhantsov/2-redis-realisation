6e7963cb4de2f39d95bbdfb95d463bb9a90d896a
ilyabukhantsov <ilyabukhantsov5@gmail.com>
2026-09-05 12:09:49 +0300

    feat: add TTL check

--- implementation/implementation.go
+++ implementation/implementation.go
@@ -3,29 +3,58 @@ package implementation
 import (
 	"errors"
 	"sync"
+	"time"
 )
+
+type Item struct {
+	value     any
+	expiresAt time.Time
+}
 
 type Data struct {
 	mu    sync.RWMutex
-	store map[any]any
+	store map[any]Item
 }
 
 func newData() *Data {
 	return &Data{
-		store: make(map[any]any),
+		store: make(map[any]Item),
 	}
 }
 
-func (d *Data) Set(key string, value any) {
+func (d *Data) Set(key string, value any, alive_minutes int16) error {
 	d.mu.Lock()
 	defer d.mu.Unlock()
 
-	d.store[key] = value
+	if alive_minutes <= 0 {
+		return errors.New("Time cannot be negative!")
+	}
+
+	item := Item{
+		value:     value,
+		expiresAt: time.Now().Add(time.Duration(alive_minutes) * time.Minute),
+	}
+
+	d.store[key] = item
+	return nil
+}
+
+func (d *Data) CheckTTL(value Item) error {
+
+	now := time.Now()
+
+	if now.After(value.expiresAt) {
+		return errors.New("TTL Expired!")
+	}
+
+	return nil
 }
 
 func (d *Data) Get(key string) (value any, err error) {
 	d.mu.RLock()
 	defer d.mu.RUnlock()
+
+	//	err := d.CheckTTL()
 
 	if val, exists := d.store[key]; exists {
 		return val, nil
